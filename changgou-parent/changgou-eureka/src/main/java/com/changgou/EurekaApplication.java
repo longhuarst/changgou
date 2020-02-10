0f781b4bb0bc11f6232e491ce253ba375787498f
luhuichen <841105197@qq.com>
2020-02-10 19:08:00 +0800

    Initial commit

---
+++ changgou-parent/changgou-eureka/src/main/java/com/changgou/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.changgou;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer  //开启eureka服务
+public class EurekaApplication {
+
+
+    /*
+    * 加载启动类， 以启动类为当前SpringBoot的配置标准
+    * */
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
